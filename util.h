5df9dfcd284f2650442d3fac987732d082412be1
Leonel Santos <franciscoleonelfds@gmail.com>
2023-12-14 08:32:22 -0300

    implementação de algumas funções pt3

--- util.h
+++ util.h
@@ -53,6 +53,45 @@ typedef struct Lista
     vitima *tail;
 }Lista;
 
+typedef struct piauiRG
+{
+    int RG;
+    char nome[60];
+    char nomePai[60];
+    char nomeMae[60];
+    char naturalidade[15];
+    int diaExpedicao;
+    int mesExpedicao;
+    int anoExpedicao;
+    char CPF[12];
+
+    struct piauiRG *proxpi;
+
+} piauiRG;
+
+typedef struct maranhaoRG
+{
+    int RG;
+    char nome[60];
+    char nomePai[60];
+    char nomeMae[60];
+    char naturalidade[15];
+    int diaExpedicao;
+    int mesExpedicao;
+    int anoExpedicao;
+    char CPF[12];
+
+    struct maranhaoRG *proxma;
+
+} maranhaoRG;
+
+typedef struct seguranca
+{
+    char login[20];
+    char servidor[20];
+    struct seguranca *prox;
+} seguranca;
+
 char* gerarNumerosAleatorios();
 vitima *RemoverVitima(Lista *lista, char *numeroProcesso);
 vitima *buscar(Lista *lista, char *conteudo);
@@ -60,6 +99,19 @@ Lista * inserirLista(Lista *ptr_registro,vitima *ptr_elementoNovo);
 void fazerBO(Lista *ptr_registroLeve,Lista *ptr_registroMedio,Lista *ptr_registroGrave);
 void buscarBO(Lista *ptr_registroLeve, Lista *ptr_registroMedio, Lista *ptr_registroGrave);
 void imprimirDetalhesDoCaso(vitima *ptr_aux);
-
+void imprimirNacionalPI(piauiRG **head, char CPFdigitado[12]);
+void CadastraInscricao(seguranca **head);
+void BuscarInscricao(seguranca **head);
+void ImprimirInscricao(seguranca **head);
+void AcessoAdmin();
+int verificadorCPFpi(piauiRG **headPi, char CPFDigitado[12]);
+int verificadorCPFma(maranhaoRG **headMa, char CPFDigitado[12]);
+void PiauiRG(piauiRG **headPI);
+void imprimirPi(piauiRG **head);
+void imprimirNacionalPI(piauiRG **head, char CPFdigitado[12]);
+void MaranhaoRg(maranhaoRG **headman);
+void imprimirMA(maranhaoRG **headma);
+void imprimirNacionalMA(maranhaoRG **head, char CPFdigitado[12]);
+void BuscarNacional(piauiRG **headPi, maranhaoRG **headMa, char CPFdigitado[12]);
 
 #endif //ATIVIDADEEXTENSAO_UTIL_H
